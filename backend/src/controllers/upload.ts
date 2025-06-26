ed635a307e07bcf8a93ce80e20fedde876db1c4c
Mitsuchi <arseniyvlasov2@gmail.com>
2025-06-27 02:51:00 +0300

    test 6

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,6 +1,9 @@
-import { NextFunction, Request, Response } from 'express'
-import { constants } from 'http2'
-import BadRequestError from '../errors/bad-request-error'
+import { Request, Response, NextFunction } from 'express';
+import sharp from 'sharp';
+import { constants } from 'http2';
+import { join } from 'path';
+import { unlink } from 'fs/promises';
+import BadRequestError from '../errors/bad-request-error';
 
 export const uploadFile = async (
     req: Request,
@@ -8,19 +11,43 @@ export const uploadFile = async (
     next: NextFunction
 ) => {
     if (!req.file) {
-        return next(new BadRequestError('Файл не загружен'))
+        return next(new BadRequestError('Файл не загружен'));
     }
-    try {
-        const fileName = process.env.UPLOAD_PATH
-            ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
-            : `/${req.file?.filename}`
-        return res.status(constants.HTTP_STATUS_CREATED).send({
-            fileName,
-            originalName: req.file?.originalname,
-        })
-    } catch (error) {
-        return next(error)
-    }
-}
 
-export default {}
+    try {
+        const tempPath = join(
+            __dirname,
+            '../public/',
+            process.env.UPLOAD_PATH_TEMP || '',
+            req.file.filename
+        );
+
+        const finalPath = join(
+            __dirname,
+            '../public/',
+            process.env.UPLOAD_PATH || '',
+            req.file.filename
+        );
+        
+        const metadata = await sharp(tempPath).metadata();
+        if (!metadata.width || !metadata.height) {
+            throw new BadRequestError('Невозможно прочитать метаданные изображения');
+        }
+        
+        await sharp(tempPath)
+            .toFormat('jpeg')
+            .toFile(finalPath);
+        
+        await unlink(tempPath);
+
+        return res.status(constants.HTTP_STATUS_CREATED).send({
+            fileName: finalPath,
+            originalName: req.file.originalname,
+            width: metadata.width,
+            height: metadata.height,
+            format: metadata.format,
+        });
+    } catch (error) {
+        return next(error);
+    }
+};

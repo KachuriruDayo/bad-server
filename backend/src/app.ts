119c398035a3bde475c97a783d30b61ca363a9a6
Mitsuchi <arseniyvlasov2@gmail.com>
2025-07-01 13:42:56 +0300

    test 10

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,15 +7,16 @@ import mongoose from 'mongoose'
 import path from 'path'
 import mongoSanitize from 'express-mongo-sanitize';
 import rateLimit from 'express-rate-limit';
+import helmet from "helmet";
 import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
 
 const globalLimiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    limit: 100,
-    keyGenerator: (req) => (req as any).user?.id || req.ip,
+    windowMs: 15 * 60 * 1000,
+    limit: 10,
+    // keyGenerator: (req) => (req as any).user?.id || req.ip,
     message: 'Слишком много запросов, попробуйте позже.',
     standardHeaders: true,
     legacyHeaders: false,
@@ -23,15 +24,19 @@ const globalLimiter = rateLimit({
 
 const { PORT = 3000 } = process.env
 const app = express()
+
 app.use(globalLimiter);
+
+app.use(helmet())
+app.set('trust proxy', true)
 
 app.use(cors({ origin: process.env.ORIGIN_ALLOW, credentials: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(serveStatic(path.join(__dirname, 'public')))
 
 
-app.use(json({ limit: '10mb' }))
-app.use(urlencoded({ limit: '10mb', extended: true }))
+app.use(json({ limit: '10kb' }))
+app.use(urlencoded({ limit: '10kb', extended: true }))
 app.use(cookieParser())
 
 app.options('*', cors({ origin: process.env.ORIGIN_ALLOW, credentials: true }));
--- backend/src/middlewares/serverStatic.ts
+++ backend/src/middlewares/serverStatic.ts
@@ -1,26 +1,34 @@
-import { NextFunction, Request, Response } from 'express'
-import fs from 'fs'
-import path from 'path'
+import { NextFunction, Request, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
 
 export default function serveStatic(baseDir: string) {
-    const absBaseDir = path.resolve(baseDir)
+    const absBaseDir = path.resolve(baseDir);
 
     return (req: Request, res: Response, next: NextFunction) => {
-        const requestedPath = path.normalize(req.path)
-
-        const filePath = path.resolve(absBaseDir, `.${  requestedPath}`) // . + чтобы избежать игнорирования baseDir при абсолютных путях
-
+        const requestedPath = path.normalize(req.path);
+        
+        const filePath = path.resolve(absBaseDir, `.${  requestedPath}`);
+        
         if (!filePath.startsWith(absBaseDir)) {
-            return next()
+            console.warn(`[serveStatic] Path Traversal attempt: ${filePath}`);
+            return res.status(403).send('Forbidden');
         }
-
-        fs.access(filePath, fs.constants.F_OK, (err) => {
+        
+        fs.stat(filePath, (err, stats) => {
             if (err) {
-                return next()
+                return next();
             }
+
+            if (!stats.isFile()) {
+                return next();
+            }
+            
             res.sendFile(filePath, (err) => {
-                if (err) next(err)
-            })
-        })
-    }
+                if (err) {
+                    next(err);
+                }
+            });
+        });
+    };
 }

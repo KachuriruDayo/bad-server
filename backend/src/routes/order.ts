270df321155d528442fc1435d2110dc0f78dfe3e
Mitsuchi <arseniyvlasov2@gmail.com>
2025-07-01 23:24:52 +0300

    test 17

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -8,27 +8,29 @@ import {
     getOrdersCurrentUser,
     updateOrder,
 } from '../controllers/order'
-import { roleGuardMiddleware } from '../middlewares/auth'
+import auth, { roleGuardMiddleware } from '../middlewares/auth'
 import { validateOrderBody } from '../middlewares/validations'
 import { Role } from '../models/user'
 
 const orderRouter = Router()
 
-orderRouter.post('/', validateOrderBody, createOrder)
-orderRouter.get('/all', roleGuardMiddleware(Role.Admin), getOrders)
-orderRouter.get('/all/me', getOrdersCurrentUser)
+orderRouter.post('/', auth, validateOrderBody, createOrder)
+orderRouter.get('/all', auth, roleGuardMiddleware(Role.Admin), getOrders)
+orderRouter.get('/all/me', auth, getOrdersCurrentUser)
 orderRouter.get(
     '/:orderNumber',
+    auth,
     roleGuardMiddleware(Role.Admin),
     getOrderByNumber
 )
-orderRouter.get('/me/:orderNumber', getOrderCurrentUserByNumber)
+orderRouter.get('/me/:orderNumber', auth, getOrderCurrentUserByNumber)
 orderRouter.patch(
     '/:orderNumber',
+    auth,
     roleGuardMiddleware(Role.Admin),
     updateOrder
 )
 
-orderRouter.delete('/:id', roleGuardMiddleware(Role.Admin), deleteOrder)
+orderRouter.delete('/:id', auth, roleGuardMiddleware(Role.Admin), deleteOrder)
 
 export default orderRouter
